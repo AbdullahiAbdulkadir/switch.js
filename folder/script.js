2f4ba77730ab49ffeb112edb0dcdc7e6dc1f9146
Abdullahiabdulkadir <Abdullahiabdulkadir0213999@gmail.com>
2022-07-12 18:25:25 +0100

    switch.js

---
+++ folder/script.js
@@ -0,0 +1,37 @@
+let day = 1;
+switch (day) {
+  case 1:
+    document.write("Monday");
+    break;
+  case 2:
+    document.write("<h2>Tuesday</h2>");
+    break;
+  case 3:
+    document.write("<h1>wensday</h1>");
+    break;
+  case 4:
+    document.write("<h2>Thursday</h2>");
+    break;
+  case 5:
+    document.write("<h2>Friday</h2>");
+    break;
+  case 6:
+    document.write("<h2>Saterday</h2>");
+    break;
+  case 7:
+    document.write("<h2>Sunday</h2>");
+    break;
+    default:
+    document.write("defualt")
+}
+
+
+
+
+
+
+
+
+
+
+// document.write("hello!");
